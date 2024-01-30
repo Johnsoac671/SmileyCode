f748d2cceabda297674c3afbb86c9eeef213f60c
Aidan Johnson <johnsoac671@gmail.com>
2024-01-29 23:07:17 -0800

    beginning implementing parse function

--- token_parser.py
+++ token_parser.py
@@ -1,12 +1,24 @@
-from nodes import FunctionDef, Literal, OperatorNode, UnaryOperatorNode
+from nodes import FunctionDef, Literal, OperatorNode, UnaryOperatorNode, NameNode
+from tokenizer import Tokenizer
     
 class Parser:
     def __init__(self):
-        self.expressions = []
-    
-    
-
-                 
-
+        self.stack = []
+        
+    def parse(self, tokenizer: Tokenizer):
+        token = next(tokenizer)
+        
+        match token.type:
             
-
+            case "STRUCTURE":
+                
+                if token.value == "📝":
+                    self.stack += token.value
+            
+            case "TYPE":
+                if token.value == "⚙️":
+                    if self.stack[-1] == "📝":
+                        self.stack.pop()
+                        
+                        func_name = next(tokenizer)
+                        
